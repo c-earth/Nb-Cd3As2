ed207244ce04ffc81b4df5c79600cab6a5d7ccc2
Abhijatmedhi Chotrattanapituk <c_earth@mit.edu>
2025-04-17 11:52:39 -0400

    detail plot alignment

--- ppms/plot.py
+++ ppms/plot.py
@@ -6,41 +6,61 @@ import matplotlib.pyplot as plt
 
 
 mpl.rcParams['text.usetex'] = True
-mpl.rcParams['font.family'] = 'serif'
-mpl.rcParams['font.serif'] = 'Times'
+mpl.rcParams['font.family'] = 'cm'
 mpl.rcParams['font.size'] = '9'
 
-# base = 1000
-# colors = mpl.colormaps['gnuplot'](np.log(np.linspace(base**(0.1), base**(0.9), 300))/np.log(base))
 colors = mpl.colormaps['gnuplot'](np.linspace(0, 1, 380))
 cmap = mpl.colors.LinearSegmentedColormap.from_list('custom_gnuplot', colors)
 
 width_pt = 246
 width_in = width_pt/72.27
-subplot_arg = (0.2, 0.25, 0.72, 0.68)
+
+def plot_arg(axis_size, width_in, axis_margin, have_label, have_tick_label, label_size, tick_label_size):
+    fig_size = [(axis_size[i] + (2 + have_label[i] * (1 - have_tick_label[i])) * axis_margin[i] + label_size[i] * have_label[i] + tick_label_size[i] * have_tick_label[i]) * width_in for i in range(2)]
+    ax_size = [((1 + have_label[i] * (1 - have_tick_label[i])) * axis_margin[i] + label_size[i] * have_label[i] + tick_label_size[i] * have_tick_label[i]) * width_in / fig_size[i] for i in range(2)] \
+            + [axis_size[i] * width_in / fig_size[i] for i in range(2)]
+    return fig_size, ax_size
 
 with open('extracted.pkl', 'rb') as f:
     data, Bs = pkl.load(f)
 
+fig_size, ax_size = plot_arg((1.78, 0.05), width_in, (0.03, 0.02), (1, 1), (1, 1), (0.05, 0.05), (0.08, 0.05))
+fig = plt.figure(figsize = fig_size)
+ax = fig.add_axes(ax_size)
+ax.axis('off')
+cb = fig.colorbar(plt.cm.ScalarMappable(cmap = cmap, norm = plt.Normalize(vmin = -30, vmax = 350)), orientation = 'horizontal', cax = ax.inset_axes([0, 0, 1, 1]))
+cb.ax.set_xlabel(r'$T$ [K]')
+cb.ax.set_xlim(0, 300)
+fig.savefig(f'Rho_colorbar.pdf', dpi = 300)
+
+fig_size, ax_size = plot_arg((0.05, 1.8), width_in, (0.07, 0.02), (0, 0), (0, 0), (0.05, 0.05), (0.08, 0.05))
+fig = plt.figure(figsize = fig_size)
+ax = fig.add_axes(ax_size)
+ax.axis('off')
+cb = fig.colorbar(plt.cm.ScalarMappable(cmap = cmap, norm = plt.Normalize(vmin = -30, vmax = 350)), orientation = 'vertical', cax = ax.inset_axes([0, 0, 1, 1]))
+cb.ax.set_ylabel(r'$T$ [K]')
+cb.ax.set_ylim(0, 300)
+fig.savefig(f'MR_colorbar.pdf', dpi = 300)
 
 for data_name in data:
     for exp, exp_data in data[data_name].items():
-        fig = plt.figure(figsize = (2*width_in/3, width_in/2))
-        ax = fig.add_axes(subplot_arg)
+        fig_size, ax_size = plot_arg((0.5, 0.5), width_in, (0.03, 0.04), (exp == 'Para', data_name == 'bulk'), (1, data_name == 'bulk'), (0.05, 0.05), (0.08, 0.05))
+        fig = plt.figure(figsize = fig_size)
+        ax = fig.add_axes(ax_size)
         
         for T, Rho in zip(np.flip(exp_data['Ts'], axis = 0), np.flip(exp_data['Rhos'], axis = 0)):
             ax.plot(Bs, Rho * 1E6, '-', color = colors[int(T)+30])
         ax.set_xlim(0, 9)
-        ax.set_xlabel('B [T]')
-        ax.set_ylabel('$\\rho$ [$\mu\Omega$ m]')
+        
+        
+        if data_name != 'bulk':
+            ax.set_xticklabels([])
+        else:
+            ax.set_xlabel('$B$ [T]')
 
-        # cb = fig.colorbar(plt.cm.ScalarMappable(cmap = cmap, norm = plt.Normalize(vmin = -30, vmax = 350)), cax = ax.inset_axes([0.5, 0.1, 0.05, 0.8]))
-        # cb.ax.set_title(r'$T$ [K]', fontsize = 10)
-        # cb.ax.tick_params(length = 5, width = 1.5, labelsize = 10, which = 'both', direction = 'out')
-        # cb.ax.set_ylim(0, 300)
-        # cb.ax.set_yscale('log')
-        # cb.ax.set_yticks([10, 100])
-        # cb.ax.set_yticklabels([10, 100])
+        if exp == 'Para':
+            ax.set_ylabel('$\\rho$ [$\mu\Omega\cdot$m]')
+            ax.yaxis.set_label_coords(-ax_size[0], 0.5)
 
         fig.savefig(f'Rho_{exp}_{data_name}.pdf', dpi = 300)
 
@@ -49,23 +69,20 @@ for data_name in data:
 
         MRs = (exp_data['Rhos'] / exp_data['Rhos'][:, :1] - 1) * 100
 
-        fig = plt.figure(figsize = (2*width_in/3, width_in/2))
-        ax = fig.add_axes(subplot_arg)
+        fig = plt.figure(figsize = fig_size)
+        ax = fig.add_axes(ax_size)
         
         for T, MR in zip(np.flip(exp_data['Ts'], axis = 0), np.flip(MRs, axis = 0)):
             ax.plot(Bs, MR, '-', color = colors[int(T)+30])
         ax.set_xlim(0, 9)
-        # ax.set_ylim(-2, 2)
-        ax.set_xlabel('B [T]')
-        ax.set_ylabel('MR [\%]')
+        if data_name != 'bulk':
+            ax.set_xticklabels([])
+        else:
+            ax.set_xlabel('$B$ [T]')
 
-        # cb = fig.colorbar(plt.cm.ScalarMappable(cmap = cmap, norm = plt.Normalize(vmin = -30, vmax = 350)), cax = ax.inset_axes([0.5, 0.1, 0.05, 0.8]))
-        # cb.ax.set_title(r'$T$ [K]', fontsize = 10)
-        # cb.ax.tick_params(length = 5, width = 1.5, labelsize = 10, which = 'both', direction = 'out')
-        # cb.ax.set_ylim(0, 300)
-        # cb.ax.set_yscale('log')
-        # cb.ax.set_yticks([10, 100])
-        # cb.ax.set_yticklabels([10, 100])
+        if exp == 'Para':
+            ax.set_ylabel('MR [\%]')
+            ax.yaxis.set_label_coords(-ax_size[0], 0.5)
 
         fig.savefig(f'MR_{exp}_{data_name}.pdf', dpi = 300)
 
